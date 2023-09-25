ebc68b9f9d51660cbc2ca218c1a67e228abf2b8e
gg <108942341+GoliasLegal@users.noreply.github.com>
2023-09-25 01:07:50 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
-clicado = () => alert("Helo world");
-mensagem =(msg) => console.log(msg);
+clicado = () => alert("Alô mundo");
+mensagem = (msg) => console.log(msg);
 soma = (a,b) => a + b;
-sub= (a,b) => a - b;
+sub = (a,b) => a - b;
 mult = (a,b) => a * b;
 div = (a,b) => a / b;
 juros_simples = (c,j) => mult(c,div(j,100));
-montante_juros_simples = (c,j) => soma(juros_simples(c,j),c);
-mensagem(soma(4,soma(7,soma(11,15))));
+montante_juro_simples = (c,j) => soma(juros_simples(c,j),c);
+
+mensagem(soma(15,soma(11,soma(4,7))));
+
 mensagem(sub(18,7));
-mensagem(soma(2,div(2,2)));
-mensagem(juros_simples(420,18))
-mensagem(montante_juros_simples(420,18))
+
+mensagem(soma(2,div(2,2)))
+/* 
+  Quanto é 18% de 420,00.
+*/
+mensagem(juros_simples(420,18));
+
+mensagem(montante_juro_simples(420,18));
